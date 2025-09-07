f33d7acd282f7235dea63016209a603d7e3f1a67
sturgis <sturgis.steele@outlook.com>
2025-09-07 13:10:49 -0500

    get grid zero education docker production ready

--- docker/init.sh
+++ docker/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
+
+SITE_NAME="${SITE_NAME:-lms.localhost}"
+ADMIN_PASSWORD="${ADMIN_PASSWORD:-admin}"
+DEVELOPER_MODE="${DEVELOPER_MODE:-0}"
+: "${MYSQL_ROOT_PASSWORD:?MYSQL_ROOT_PASSWORD env not set}"
 
 if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     echo "Bench already exists, skipping init"
     cd frappe-bench
-    bench start
+    exec bench start
 else
     echo "Creating new bench..."
 fi
@@ -11,7 +17,6 @@ fi
 export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
 
 bench init --skip-redis-config-generation frappe-bench
-
 cd frappe-bench
 
 # Use containers instead of localhost
@@ -26,15 +31,21 @@ sed -i '/watch/d' ./Procfile
 
 bench get-app lms
 
-bench new-site lms.localhost \
---force \
---mariadb-root-password 123 \
---admin-password admin \
---no-mariadb-socket
+bench new-site "${SITE_NAME}" \
+  --force \
+  --mariadb-root-password "${MYSQL_ROOT_PASSWORD}" \
+  --admin-password "${ADMIN_PASSWORD}" \
+  --no-mariadb-socket
 
-bench --site lms.localhost install-app lms
-bench --site lms.localhost set-config developer_mode 1
-bench --site lms.localhost clear-cache
-bench use lms.localhost
+bench --site "${SITE_NAME}" install-app lms
+bench --site "${SITE_NAME}" set-config developer_mode "${DEVELOPER_MODE}"
 
-bench start
+# Optional: bind a public host if provided
+if [ -n "${SITE_HOST:-}" ]; then
+  bench --site "${SITE_NAME}" set-config host_name "${SITE_HOST}"
+fi
+
+bench --site "${SITE_NAME}" clear-cache
+bench use "${SITE_NAME}"
+
+exec bench start
